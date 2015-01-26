49d1a74b2861fa509257c2aa66ea0b2a333e2b0b
khangvm53 <khangvm530@gmail.com>
2015-01-26 14:56:40 +0700

    update install file

---
+++ pkg_virtuemart_zooex.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package      Zo2
+ *
+ * @author       ZooTemplate.com
+ * @copyright    Copyright (C) 2008 - 2015. All rights reserved.
+ * @license      GPL v2 or later
+ */
+defined('_JEXEC') or die();
+class PkgVirtuemartZooExInstallerScript
+{
+    /**
+     * Called before any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function preflight($route, JAdapterInstance $adapter)
+    {
+        return true;
+    }
+    /**
+     * Called after any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function postflight($route, JAdapterInstance $adapter)
+    {
+        return true;
+    }
+}
