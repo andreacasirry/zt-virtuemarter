a3e4f0837b8be6ebd920adb304e5908d11887dea
thuan1234 <codejomla@gmail.com>
2016-04-08 13:48:28 +0700

    Fixed thumbnail issue

--- modules/mod_zt_comparelist/tmpl/default.php
+++ modules/mod_zt_comparelist/tmpl/default.php
@@ -38,12 +38,8 @@ if (plgSystemZtvirtuemarter::getZtvirtuemarterSetting()->enable_compare == '1') 
                              class="modcompareprod clearfix">
                             <div class="compare-product-img">
                                 <a href="<?php echo JRoute::_('index.php?option=com_virtuemart&view=productdetails&virtuemart_product_id=' . $product->virtuemart_product_id . '&virtuemart_category_id=' . $product->virtuemart_category_id); ?>">
-                                    <img src="<?php if (!empty($product->file_url_thumb)) {
-                                        echo JURI::base() . $product->file_url_thumb;
-                                    } else {
-                                        echo JURI::base() . 'images/stories/virtuemart/noimage.gif';
-                                    } ?>" alt="<?php echo $product->product_name; ?>"
-                                         title="<?php echo $product->product_name; ?>"/>
+                                    <?php echo $product->images[0]->displayMediaThumb('alt="' . $product->product_name . '" title="' . $product->product_name . '" ', FALSE) ; ?>
+
                                 </a>
                             </div>
                             <div class="compare-product-detail">
