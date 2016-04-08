4ba2e1fa8a776d4670966ac790336d68b94af424
thuan1234 <codejomla@gmail.com>
2016-04-08 13:56:54 +0700

    Update tab layout

--- modules/mod_ztvirtuemarter_product/mod_ztvirtuemarter_product.php
+++ modules/mod_ztvirtuemarter_product/mod_ztvirtuemarter_product.php
@@ -12,12 +12,13 @@ VmConfig::loadJLang('mod_ztvirtuemarter_product', true);
 $jinput = JFactory::getApplication()->input;
 
 //add js
-JFactory::getDocument()->addScript('modules/mod_ztvirtuemarter_product/assets/js/owl.carousel.min.js');
+JFactory::getDocument()->addScript(JURL_ROOT.'/modules/mod_ztvirtuemarter_product/assets/js/owl.carousel.min.js');
 
 // Setting
 $maxItems = $params->get('max_items', 2); //maximum number of items to display
 $layout = $params->get('layout', 'default');
-$categoryId = $params->get('virtuemart_category_id', null); // Display products from this category only
+$categoryId = $params->get('virtuemart_category_ids', null); // Display products from this category only
+$categoryIds = $params->get('virtuemart_category_ids', null);
 $filterCategory = (bool)$params->get('filter_category', 0); // Filter the category
 $displayStyle = $params->get('display_style', "div"); // Display Style
 $productsPerRow = $params->get('products_per_row', 1); // Display X products per Row
@@ -25,9 +26,10 @@ $showPrice = (bool)$params->get('show_price', 1); // Display the Product Price?
 $showAddtocart = (bool)$params->get('show_addtocart', 1); // Display the "Add-to-Cart" Link?
 $headerText = $params->get('headerText', ''); // Display a Header Text
 $footerText = $params->get('footerText', ''); // Display a footerText
-$productGroup = $params->get('product_group', 'featured'); // Display a footerText
+//$productGroup = $params->get('product_group', 'featured'); // Display a footerText
+$productGroup = ($layout == 'tab') ? $params->get('product_group_tab', array('latest')) : $params->get('product_group', 'featured'); // Display a footerText
 $newProductFrom = $params->get('new_product_from', '7');
-$mainframe = Jfactory::getApplication();
+$mainframe = JFactory::getApplication();
 $virtuemartCurrencyId = $mainframe->getUserStateFromRequest("virtuemart_currency_id", 'virtuemart_currency_id', vRequest::getInt('virtuemart_currency_id', 0));
 
 
@@ -38,12 +40,30 @@ $vendorId = vRequest::getInt('vendorid', 1);
 
 if ($filterCategory) $filterCategory = TRUE;
 
-$productModel = VmModel::getModel('Product');
+//$productModel = VmModel::getModel('Product');
+//
+//$products = $productModel->getProductListing($productGroup, $maxItems, $showPrice, true, false, $filterCategory, $categoryId);
+//$productModel->addImages($products);
+//
+//$totalProd = count($products);
 
-$products = $productModel->getProductListing($productGroup, $maxItems, $showPrice, true, false, $filterCategory, $categoryId);
-$productModel->addImages($products);
+//get product
+$products = array();
+if($layout == 'tab') {
+    if(count($productGroup) > 0)
+        foreach($productGroup as $group) {
+            $products[$group] = ModZtvirtuemarterProductHelper::getProducts($group, $maxItems, $showPrice, $filterCategory, $categoryIds);
+        }
+    if(count($categoryIds) > 0)
+        foreach ($categoryIds as $categoryId) {
+            $products[$categoryId] = ModZtvirtuemarterProductHelper::getProducts('latest', $maxItems, $showPrice, true, $categoryId);
+        }
+} else {
+    $products = ModZtvirtuemarterProductHelper::getProducts($productGroup, $maxItems, $showPrice, $filterCategory, $categoryIds);
+    $totalProd = count($products);
+}
 
-$totalProd = count($products);
+
 if (empty($products)) return false;
 $currency = CurrencyDisplay::getInstance();
 
